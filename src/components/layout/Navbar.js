4c8705572eaeb43c3878d8af345716e392885487
pranavmodx <pranavmodx@gmail.com>
2019-07-01 18:34:10 +0530

    refactored Navbar.js - moved outer functions to component's render method. The required data is obtained from context

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,40 +1,12 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { Consumer } from "../../context";
-import greData from "../../gredata.json";
+// import greData from "../../gredata.json";
 import Autosuggest from "react-autosuggest";
 
 import "../layout/Navbar.css";
 
-let words = greData;
-
-function escapeRegexCharacters(str) {
-  return str.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
-}
-
-function getSuggestions(value) {
-  const escapedValue = escapeRegexCharacters(value.trim());
-
-  if (escapedValue === "") {
-    return [];
-  }
-
-  const regex = new RegExp("^" + escapedValue, "i");
-
-  return words.filter(word => regex.test(word.WORD));
-}
-
-function getSuggestionValue(suggestion) {
-  return suggestion.WORD;
-}
-
-function renderSuggestion(suggestion) {
-  return <span>{suggestion.WORD}</span>;
-}
-
-function shouldRenderSuggestions(value) {
-  return value.trim().length > 1;
-}
+// let words = greData;
 
 class Navbar extends Component {
   constructor() {
@@ -46,50 +18,79 @@ class Navbar extends Component {
     };
   }
 
-  onChange = (event, { newValue, method }) => {
-    this.setState({
-      value: newValue
-    });
-  };
-
-  onSuggestionsFetchRequested = ({ value }) => {
-    this.setState({
-      suggestions: getSuggestions(value)
-    });
-  };
-
-  onSuggestionsClearRequested = () => {
-    this.setState({
-      suggestions: []
-    });
-  };
-
-  onKeyPress = (dispatch, e) => {
-    if (e.key === "Enter") {
-      e.preventDefault();
-      // console.log(e.target.value);
-      for (let i = 0; i < words.length; i++) {
-        if (words[i].WORD.toLowerCase() === e.target.value.toLowerCase()) {
-          // console.log("YES");
-          dispatch({ type: "SHOW_SEARCH_RESULT", payload: i });
-          break;
-        }
-      }
-    }
-  };
-
   render() {
     return (
       <Consumer>
-        {value2 => {
-          const { dispatch } = value2;
+        {contextValue => {
+          const { words, dispatch } = contextValue;
+          const onKeyPress = (dispatch, e) => {
+            if (e.key === "Enter") {
+              e.preventDefault();
+              // console.log(e.target.value);
+              for (let i = 0; i < words.length; i++) {
+                if (words[i].WORD.toLowerCase() === e.target.value.toLowerCase()) {
+                  // console.log("YES");
+                  dispatch({ type: "SHOW_SEARCH_RESULT", payload: i });
+                  break;
+                }
+              }
+            }
+          };
           const { value, suggestions } = this.state;
+
+          const onChange = (event, { newValue, method }) => {
+            this.setState({
+              value: newValue
+            });
+          };
+
           const inputProps = {
             placeholder: "Search Word",
             value,
-            onChange: this.onChange,
-            onKeyPress: this.onKeyPress.bind(this, dispatch)
+            onChange: onChange,
+            onKeyPress: onKeyPress.bind(this, dispatch)
           };
+
+          function escapeRegexCharacters(str) {
+            return str.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+          }
+          
+          function getSuggestions(value) {
+            const escapedValue = escapeRegexCharacters(value.trim());
+          
+            if (escapedValue === "") {
+              return [];
+            }
+          
+            const regex = new RegExp("^" + escapedValue, "i");
+          
+            return words.filter(word => regex.test(word.WORD));
+          }
+          
+          function getSuggestionValue(suggestion) {
+            return suggestion.WORD;
+          }
+          
+          function renderSuggestion(suggestion) {
+            return <span>{suggestion.WORD}</span>;
+          }
+          
+          function shouldRenderSuggestions(value) {
+            return value.trim().length > 1;
+          }
+        
+          const onSuggestionsFetchRequested = ({ value }) => {
+            this.setState({
+              suggestions: getSuggestions(value)
+            });
+          };
+        
+          const onSuggestionsClearRequested = () => {
+            this.setState({
+              suggestions: []
+            });
+          };
+
           return (
             <React.Fragment>
               <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-5">
@@ -123,10 +124,10 @@ class Navbar extends Component {
                       <Autosuggest
                         suggestions={suggestions}
                         onSuggestionsFetchRequested={
-                          this.onSuggestionsFetchRequested
+                          onSuggestionsFetchRequested
                         }
                         onSuggestionsClearRequested={
-                          this.onSuggestionsClearRequested
+                          onSuggestionsClearRequested
                         }
                         getSuggestionValue={getSuggestionValue}
                         renderSuggestion={renderSuggestion}
